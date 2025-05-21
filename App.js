11f3631ea7e1b5872019d9c439f3fbfa5d79e0d7
azamatziyatdinov <133596145+azamatziyatdinov@users.noreply.github.com>
2025-05-22 01:51:31 +0500

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div style={{ fontFamily: "sans-serif", textAlign: "center", padding: "2rem" }}>
       <h1>Agile Diagnostika works!</h1>
-      <p>Now everything is configured correctly.</p>
+      <p>Everything is configured correctly.</p>
     </div>
   );
 }
