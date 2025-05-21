6f50c03a2319c84268655a0b32b481a36b52446b
azamatziyatdinov <133596145+azamatziyatdinov@users.noreply.github.com>
2025-05-22 01:54:49 +0500

    Update App.js
    в

--- App.js
+++ App.js
@@ -3,8 +3,8 @@ import React from "react";
 export default function App() {
   return (
     <div style={{ fontFamily: "sans-serif", textAlign: "center", padding: "2rem" }}>
-      <h1>Agile Diagnostika works!</h1>
-      <p>Everything is configured correctly.</p>
+        <h1>Agile Diagnostika works!</h1>
+        <p>Everything is configured correctly.</p>
     </div>
   );
 }
